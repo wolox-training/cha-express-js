ae7688172e5988cb2aa889fd72bc5e73f7db7bb6
Christian Angelone <christian.angelone@wolox.com.ar>
2018-07-31 11:43:07 -0300

    Fixes more linting errors on user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,12 +1,15 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    firstname: { type: DataTypes.STRING, allowNull: false, required: true },
-    lastname: { type: DataTypes.STRING, allowNull: false, required: true },
-    email: { type: DataTypes.STRING, allowNull: false, required: true, unique: true },
-    password: { type: DataTypes.STRING, allowNull: false, required: true }
-  },
+  const User = sequelize.define(
+    'User',
+    {
+      id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+      firstname: { type: DataTypes.STRING, allowNull: false, required: true },
+      lastname: { type: DataTypes.STRING, allowNull: false, required: true },
+      email: { type: DataTypes.STRING, allowNull: false, required: true, unique: true },
+      password: { type: DataTypes.STRING, allowNull: false, required: true }
+    },
     {}
   );
   User.associate = function(models) {
