78b229a9180fb54ad22efa4ddea39366b83a3ac7
Christian Angelone <christian.angelone@wolox.com.ar>
2018-08-01 11:55:46 -0300

    Adds password hashing when creating user on controller

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,23 +1,27 @@
 'use strict';
 
+const bcrypt = require('bcryptjs');
 const User = require('../models').User;
 
 exports.create = (req, res, next) => {
   const userObj = req.body || {};
   User.isPwdLengthGreater(userObj, 8)
     .then(() => {
-      User.createModel(userObj)
-        .then(createdUser => {
-          res.status(201).send();
-        })
-        .catch(err => {
-          if (err.name === 'SequelizeValidationError') {
-            res.status(400).json({
-              errors: err.errors
-            });
-          }
-          res.status(500).send();
-        });
+      bcrypt.hash(userObj.password, 10).then(hashedPwd => {
+        userObj.password = hashedPwd;
+        User.createModel(userObj)
+          .then(createdUser => {
+            res.status(201).send();
+          })
+          .catch(err => {
+            if (err.name === 'SequelizeValidationError') {
+              res.status(400).json({
+                errors: err.errors
+              });
+            }
+            res.status(500).send();
+          });
+      });
     })
     .catch(() => {
       res.status(400).json({
