e507dd74edd99b9842820cae73f6ffb6c4d12d94
Christian Angelone <christian.angelone@wolox.com.ar>
2018-08-01 12:12:22 -0300

    Adds logging messages for user creation

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const bcrypt = require('bcryptjs');
+const logger = require('../logger');
 const User = require('../models').User;
 
 exports.create = (req, res, next) => {
@@ -11,6 +12,10 @@ exports.create = (req, res, next) => {
         userObj.password = hashedPwd;
         User.createModel(userObj)
           .then(createdUser => {
+            logger.log({
+              level: 'info',
+              message: createdUser.firstname
+            });
             res.status(201).send();
           })
           .catch(err => {
@@ -19,17 +24,26 @@ exports.create = (req, res, next) => {
                 errors: err.errors
               });
             }
+            logger.log({
+              level: 'error',
+              message: JSON.stringify(err.errors, null, 2)
+            });
             res.status(500).send();
           });
       });
     })
     .catch(() => {
+      const someErrors = [
+        {
+          error: 'password should be greater than or equal to 8 characters'
+        }
+      ];
+      logger.log({
+        level: 'error',
+        message: JSON.stringify(someErrors, null, 2)
+      });
       res.status(400).json({
-        errors: [
-          {
-            error: 'password should be greater than or equal to 8 characters'
-          }
-        ]
+        errors: someErrors
       });
     });
 };
