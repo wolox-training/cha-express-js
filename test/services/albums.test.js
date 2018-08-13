284a2866052cfa4418e8151831ea753ff239ca33
Christian Angelone <christian.angelone@wolox.com.ar>
2018-08-13 17:02:29 -0300

    Covers that service album could not find album with certain 'id'

--- test/services/albums.test.js
+++ test/services/albums.test.js
@@ -50,7 +50,7 @@ describe('AlbumsService', () => {
 
   describe('getById', () => {
     const id = 2;
-    it(`Should not retrieve an album with id: ${id}`, done => {
+    it(`Should retrieve an album with id: ${id}`, done => {
       nock(AlbumsService.URL)
         .get(`/albums/${id}`)
         .reply(200, AlbumsService.ALBUM_SAMPLE(id));
@@ -61,6 +61,20 @@ describe('AlbumsService', () => {
           done();
         })
         .catch(err => done(new Error(`Album not retrieved: ${err.message}`)));
+    });
+
+    const notFoundId = 10;
+    it(`Should not retrieve an album with id: ${notFoundId}`, done => {
+      nock(AlbumsService.URL)
+        .get(`/albums/${notFoundId}`)
+        .reply(404);
+
+      AlbumsService.getById(notFoundId)
+        .then(albums => done(new Error('Successful response - should not be called')))
+        .catch(err => {
+          err.response.should.have.status(404);
+          done();
+        });
     });
   });
 });
