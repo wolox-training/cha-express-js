85570966f526e95fe95b15d1e518c5c644e68f4a
Christian Angelone <christian.angelone@wolox.com.ar>
2018-08-15 10:28:55 -0300

    Covers that album service retrives photos for a given id

--- app/services/albums.js
+++ app/services/albums.js
@@ -27,10 +27,40 @@ exports.ALBUMS_SAMPLE = [
 
 exports.ALBUM_SAMPLE = id => exports.ALBUMS_SAMPLE[id - 1];
 
+exports.PHOTOS_FOR_ALBUM_SAMPLE = id => {
+  return [
+    {
+      albumId: id,
+      id: 1,
+      title: 'accusamus beatae ad facilis cum similique qui sunt',
+      url: 'http://placehold.it/600/92c952',
+      thumbnailUrl: 'http://placehold.it/150/92c952'
+    },
+    {
+      albumId: id,
+      id: 2,
+      title: 'reprehenderit est deserunt velit ipsam',
+      url: 'http://placehold.it/600/771796',
+      thumbnailUrl: 'http://placehold.it/150/771796'
+    },
+    {
+      albumId: id,
+      id: 3,
+      title: 'officia porro iure quia iusto qui ipsa ut modi',
+      url: 'http://placehold.it/600/24f355',
+      thumbnailUrl: 'http://placehold.it/150/24f355'
+    }
+  ];
+};
+
 exports.all = () => {
   return axios(`${exports.URL}/albums`).then(res => res.data);
 };
 
 exports.getById = id => {
   return axios(`${exports.URL}/albums/${id}`).then(res => res.data);
+};
+
+exports.getPhotosForAlbumWithId = id => {
+  return axios(`${exports.URL}/albums/${id}/photos`).then(res => res.data);
 };
