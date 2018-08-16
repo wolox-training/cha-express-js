20ac78fb403fc3a3349fad2378a1c7035623b22d
Christian Angelone <christian.angelone@wolox.com.ar>
2018-08-16 19:08:39 -0300

    Covers that a stored token in tokens repo is an active token

--- app/services/tokens_repo.js
+++ app/services/tokens_repo.js
@@ -2,4 +2,15 @@ const tokensRepo = [];
 
 exports.numberOfTokens = () => tokensRepo.length;
 
-exports.store = token => tokensRepo.push(token);
+exports.store = token => {
+  tokensRepo.push({
+    token,
+    active: true
+  });
+};
+
+exports.isActive = token => {
+  const foundToken = tokensRepo.find(record => record.token === token);
+  const active = foundToken ? foundToken.active : false;
+  return active;
+};
--- test/services/tokens_repo.test.js
+++ test/services/tokens_repo.test.js
@@ -6,15 +6,23 @@ chai.use(chaiThings);
 
 const TokensRepoService = require('../../app/services/tokens_repo');
 
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6InJlZ3VsYXIiLCJleHAiOjE1MzQ0NDU5MTcsImlhdCI6MTUzNDQ0NTg1N30.xwBZB6ioUZ-Blp08tV_zLrXvAy8kGlQGfo_OrwrUFhU';
+
 describe('TokensRepoService', () => {
   describe('store', () => {
     it('Should store a token', done => {
       const numberOfTokensBefore = TokensRepoService.numberOfTokens();
-      const token =
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6InJlZ3VsYXIiLCJleHAiOjE1MzQ0NDU5MTcsImlhdCI6MTUzNDQ0NTg1N30.xwBZB6ioUZ-Blp08tV_zLrXvAy8kGlQGfo_OrwrUFhU';
       TokensRepoService.store(token);
       const numberOfTokensAfter = TokensRepoService.numberOfTokens();
       numberOfTokensAfter.should.equal(numberOfTokensBefore + 1);
+      done();
+    });
+
+    it('Should stored token be active', done => {
+      TokensRepoService.store(token);
+      const active = TokensRepoService.isActive(token);
+      active.should.equal(true);
       done();
     });
   });
